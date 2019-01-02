9a7729e0580ada6eed2d8300b11dc51df4c683e8
Alex Harden <alex.harden@allscripts.com>
2019-01-02 14:43:12 -0500

    Update GitHub Action

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -9,4 +9,11 @@ if [ $SUCCESS -eq 0 ]; then
     exit 0
 fi
 
+COMMENT="#### \`ansible-lint\` Failed
+$LINT_ACTION
+"
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+
 exit $SUCCESS
